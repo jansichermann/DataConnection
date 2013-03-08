be3fa93db3c2eb120e8a6c6f79fcfb3a9c7857c2
Jan Sichermann <jan@urbancompass.com>
2013-03-08 17:14:28 -0500

    adding demo project

---
+++ Demo/src/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DataConnectionDemo
+//
+//  Created by jan on 3/8/13.
+//  Copyright (c) 2013 jansichermann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
